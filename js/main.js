7418bfde37bb4e22259e74fd803462da1a33069f
Brayan Estiven Andrade Carrillo <brayandrade33@gmail.com>
2025-09-22 17:46:30 -0500

    Se agrego funcionalidad del panel lateral, con funciones y js terminados, falta suma y resta del total

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const input = document.getElementsByClassName("form-control");
 const contCard = document.getElementById("cont-cards-fila");
 const form = document.getElementById("vehiculo-form")
+const contCarrito = document.getElementById("cont-productos");
 
 function createCard(url, nombreA, marcaA, modeloA, kilometrajeA, precioA) {
 
@@ -115,9 +116,23 @@ function eventsCars(cardPrincipal) {
     const btnComprar = cardPrincipal.querySelector("#comprar");
     const btnDelete = cardPrincipal.querySelector("#eliminar");
     let nombreV = cardPrincipal.querySelector("#nombre").textContent;
+    let url = cardPrincipal.querySelector("img").getAttribute("src");
+    let marca = cardPrincipal.querySelector(".text-muted").textContent;
+    let precio = cardPrincipal.querySelector(".text-success").textContent; 
 
-    btnComprar.addEventListener('click', () => {
+    btnComprar.addEventListener('click', (e) => {
+
+        e.preventDefault();
+
         alert("Has comprado el vehiculo " + nombreV + "\nGracias por su compra!!!! ");
+        
+
+        const newProducto = CreateProducto(url, nombreV, marca, precio);
+        contCarrito.appendChild(newProducto);
+        actualizarTotal(precio);
+        deleteProductoCarrito(newProducto);
+
+
     });
 
     btnDelete.addEventListener("click", () => {
@@ -136,3 +151,77 @@ function modalProductos(){
 document.addEventListener('DOMContentLoaded', function() { 
 document.getElementById('carrito').addEventListener('click', modalProductos);
 });
+
+function CreateProducto(url, nombreV, marca, precio) {
+    
+    const carritoPrincipal = document.createElement("div");
+    carritoPrincipal.classList.add("productos-card");
+
+    const carritoFila = document.createElement("div");
+    carritoFila.classList.add("row");
+
+    const colCarrito1 = document.createElement("div");
+    colCarrito1.classList.add("col-lg-4","col-md-4","col-carrito1");
+
+    const imgCarrito = document.createElement("img");
+    imgCarrito.classList.add("w-100");
+    imgCarrito.setAttribute("alt", "Foto vehículo");
+    imgCarrito.setAttribute("src", url);
+
+    const colCarrito2 = document.createElement("div");
+    colCarrito2.classList.add("col-lg-6","col-md-6","col-carrito2");
+
+    const h3Nombre = document.createElement("h3");
+    h3Nombre.classList.add("card-title");
+    h3Nombre.textContent = nombreV;
+
+    const h4Marca = document.createElement("h4");
+    h4Marca.classList.add("card-subtitle", "text-muted");
+    h4Marca.textContent = marca;
+
+    const h3Precio = document.createElement("h3");
+    h3Precio.classList.add("text-success");
+    h3Precio.textContent = precio;
+
+
+    const colCarrito3 = document.createElement("div");
+    colCarrito3.classList.add("col-lg-2","col-md-2","col-carrito3");
+
+    const btndelete = document.createElement("button");
+    btndelete.classList.add("btn", "btn-danger");
+    btndelete.setAttribute("id", "eliminar-carrito");
+    btndelete.textContent = "X";
+
+
+    carritoPrincipal.appendChild(carritoFila);
+    carritoFila.appendChild(colCarrito1);
+    colCarrito1.appendChild(imgCarrito);
+    carritoFila.appendChild(colCarrito2);
+    colCarrito2.appendChild(h3Nombre);
+    colCarrito2.appendChild(h4Marca);
+    colCarrito2.appendChild(h3Precio);
+    carritoFila.appendChild(colCarrito3)
+    colCarrito3.appendChild(btndelete);
+
+    return carritoPrincipal
+
+
+}
+
+
+
+function deleteProductoCarrito(carritoPrincipal) {
+
+    const btnDelete = carritoPrincipal.querySelector("#eliminar-carrito");
+
+    btnDelete.addEventListener("click", () => {
+        carritoPrincipal.remove();
+    });
+    
+}
+
+
+//suma de total
+
+let totalCarrito = 0;
+
